0529782d8e26800f14ed10015dfa9c432cadbeaa
Karol Herbst <kherbst@redhat.com>
2018-06-20 12:00:40 +0200

    nouveau hack

--- src/gallium/drivers/nouveau/codegen/nv50_ir_from_nir.cpp
+++ src/gallium/drivers/nouveau/codegen/nv50_ir_from_nir.cpp
@@ -2220,6 +2220,7 @@ Converter::visit(nir_intrinsic_instr *insn)
 
       break;
    }
+   case nir_intrinsic_load_private:
    case nir_intrinsic_load_global: {
       const DataType dType = getDType(insn);
       LValues &newDefs = convert(&insn->dest);
@@ -2232,6 +2233,7 @@ Converter::visit(nir_intrinsic_instr *insn)
       info->io.globalAccess |= 0x2;
       break;
    }
+   case nir_intrinsic_store_private:
    case nir_intrinsic_store_global: {
       DataType sType = getSType(insn->src[0], false, false);
 
