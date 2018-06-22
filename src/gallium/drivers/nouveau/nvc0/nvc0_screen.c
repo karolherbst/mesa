271503f9780ce0057af35c0990d8012419b35fd5
Karol Herbst <kherbst@redhat.com>
2018-06-22 23:35:36 +0200

    nvc0: enable 4.6

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -130,7 +130,7 @@ nvc0_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_GLSL_FEATURE_LEVEL:
       return 460;
    case PIPE_CAP_GLSL_FEATURE_LEVEL_COMPATIBILITY:
-      return 430;
+      return 460;
    case PIPE_CAP_MAX_RENDER_TARGETS:
       return 8;
    case PIPE_CAP_MAX_DUAL_SOURCE_RENDER_TARGETS:
