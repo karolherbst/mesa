1d795b061ab95e2e1124f977d0d5a76ccc3f687f
Pierre Moreau <dev@pmoreau.org>
2020-05-05 13:16:42 +0200

    clover: Implement clCreateProgramWithILKHR
    Signed-off-by: Pierre Moreau <dev@pmoreau.org>

--- src/gallium/frontends/clover/core/program.hpp
+++ src/gallium/frontends/clover/core/program.hpp
@@ -38,11 +38,16 @@ namespace clover {
          evals, const std::vector<intrusive_ref<device>> &> device_range;
 
    public:
+      enum class il_type { none, spirv };
+
       program(clover::context &ctx,
               const std::string &source);
       program(clover::context &ctx,
               const ref_vector<device> &devs = {},
               const std::vector<module> &binaries = {});
+      program(clover::context &ctx,
+              std::vector<char> &&il,
+              enum il_type il_type);
 
       program(const program &prog) = delete;
       program &
@@ -55,6 +60,10 @@ namespace clover {
 
       const bool has_source;
       const std::string &source() const;
+
+      const bool has_il;
+      const std::vector<char> &il() const;
+      enum il_type il_type() const;
 
       device_range devices() const;
 
@@ -85,6 +94,8 @@ namespace clover {
       std::map<const device *, struct build> _builds;
       std::string _source;
       ref_counter _kernel_ref_counter;
+      std::vector<char> _il;
+      enum il_type _il_type;
    };
 }
 
