67550da084cfdb7ff7a71cbe2c5ae6bd00212731
Karol Herbst <kherbst@redhat.com>
2020-06-17 03:05:54 +0200

    nv50/ir/nir: handle clip vertex for tess eval shaders
    Signed-off-by: Karol Herbst <kherbst@redhat.com>
    Tested-by: Ben Skeggs <bskeggs@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5512>

--- src/gallium/drivers/nouveau/codegen/nv50_ir_from_nir.cpp
+++ src/gallium/drivers/nouveau/codegen/nv50_ir_from_nir.cpp
@@ -1665,6 +1665,7 @@ Converter::visit(nir_intrinsic_instr *insn)
             break;
          }
          case Program::TYPE_GEOMETRY:
+         case Program::TYPE_TESSELLATION_EVAL:
          case Program::TYPE_VERTEX: {
             if (info->io.genUserClip > 0 && idx == (uint32_t)clipVertexOutput) {
                mkMov(clipVtx[i], src);
