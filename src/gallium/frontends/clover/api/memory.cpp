c598b5a4de5d1cd4b7d98c3b7d51acf66986665b
Karol Herbst <kherbst@redhat.com>
2020-08-20 17:05:13 +0200

    clover: track allocated svm pointers
    We need those to proper validate the SVM API.

--- src/gallium/frontends/clover/api/memory.cpp
+++ src/gallium/frontends/clover/api/memory.cpp
@@ -549,6 +549,10 @@ clSVMAlloc(cl_context d_ctx,
       if (alignment < sizeof(void*))
          alignment = sizeof(void*);
       posix_memalign(&ptr, alignment, size);
+
+      if (ptr)
+         ctx.add_svm_allocation(ptr, size);
+
       return ptr;
    }
 
@@ -565,8 +569,10 @@ clSVMFree(cl_context d_ctx,
    auto &ctx = obj(d_ctx);
    bool can_emulate = all_of(std::mem_fn(&device::has_system_svm), ctx.devices());
 
-   if (can_emulate)
+   if (can_emulate) {
+      ctx.remove_svm_allocation(svm_pointer);
       return free(svm_pointer);
+   }
 
    CLOVER_NOT_SUPPORTED_UNTIL("2.0");
 
