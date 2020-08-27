7236ad69b5809a431aa5a1e688b5c3a9be593f8b
Jason Ekstrand <jason@jlekstrand.net>
2020-08-27 15:56:38 -0500

    clover/nir: Use lower_vars_to_explicit for uniform and global
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>

--- src/gallium/frontends/clover/nir/invocation.cpp
+++ src/gallium/frontends/clover/nir/invocation.cpp
@@ -336,16 +336,9 @@ module clover::nir::spirv_to_nir(const module &mod, const device &dev,
       NIR_PASS_V(nir, clover_lower_nir, args, dev.max_block_size().size(),
                  dev.address_bits());
 
-      // Calculate input offsets.
-      unsigned offset = 0;
-      nir_foreach_uniform_variable(var, nir) {
-         offset = align(offset, glsl_get_cl_alignment(var->type));
-         var->data.driver_location = offset;
-         offset += glsl_get_cl_size(var->type);
-      }
-
       NIR_PASS_V(nir, nir_lower_vars_to_explicit_types,
-                 nir_var_mem_shared | nir_var_function_temp,
+                 nir_var_uniform | nir_var_mem_shared |
+                 nir_var_mem_global | nir_var_function_temp,
                  glsl_get_cl_type_size_align);
 
       /* use offsets for kernel inputs (uniform) */
